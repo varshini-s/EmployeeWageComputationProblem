bb1b26ee15756436b67149e8d38a5f338db8239e
varshini <dreamvarshini@gmail.com>
2021-08-20 14:16:15 +0530

    MODIFIED:new use case UC1

--- src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
@@ -6,6 +6,20 @@ public class EmployeeWageCalculator {
 	
 	System.out.println("*-------------Welcome to Employee Wage Computation Program-------------*");
 	
+	//Constants
+	int IS_FULL_TIME=1;
+	//Computations
+	double empCheck=Math.floor(Math.random()*10)%2;
+	
+	if(IS_FULL_TIME==empCheck)
+	{
+		System.out.println("Employee is Present");
+	}
+	else
+	{
+		System.out.println("Employee is absent");
+	}
+	
 	}
 	
 
