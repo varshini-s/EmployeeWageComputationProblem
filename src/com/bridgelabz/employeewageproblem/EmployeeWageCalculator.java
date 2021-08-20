09668773fa59734273f3a845c6e9945bb1a0cc59
varshini <dreamvarshini@gmail.com>
2021-08-20 14:37:27 +0530

    MODIFIED:new use case UC4

--- src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
@@ -1,35 +1,34 @@
 package com.bridgelabz.employeewageproblem;
 
 public class EmployeeWageCalculator {
-	
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
 	public static void main(String args[]) {
 	
 	System.out.println("*-------------Welcome to Employee Wage Computation Program-------------*");
 	
-	//Constants
-	int IS_FULL_TIME=1;
-	int IS_PART_TIME=2;
-	int EMP_RATE_PER_HOUR=20;
 	
 	//variables
 	int empHours=0;
 	int empWage=0;
 	
 	//Computations
-	double empCheck=Math.floor(Math.random()*10)%3;
+	int empCheck=(int)Math.floor(Math.random()*10)%3;
 	
-	if(IS_FULL_TIME==empCheck)
-	{
+	switch(empCheck) {
+	
+	case IS_FULL_TIME:
 		empHours=8;
-	}
-	else if(IS_PART_TIME==empCheck)
-	{
+		break;
+	case IS_PART_TIME:
 		empHours=4;
-	}
-	else
-	{
+		break;
+	default:
 		empHours=0;
+	
 	}
+	
 	empWage=empHours*EMP_RATE_PER_HOUR;
 	System.out.println("Employee wage:"+empWage);
 	
