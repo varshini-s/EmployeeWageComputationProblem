2bbce62b9750367fa928bb23dd77383ae5cc89d5
varshini <dreamvarshini@gmail.com>
2021-08-20 13:42:18 +0530

    ADDED:Welcome message

---
+++ src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.employeewageproblem;
+
+public class EmployeeWageCalculator {
+	
+	public static void main(String args[]) {
+	
+	System.out.println("*-------------Welcome to Employee Wage Computation Program-------------*");
+	
+	}
+	
+
+}
