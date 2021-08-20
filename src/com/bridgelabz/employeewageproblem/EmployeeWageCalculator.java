0418f74f94542081afd4ff40b503f1c472278ad1
varshini <dreamvarshini@gmail.com>
2021-08-20 14:46:04 +0530

    MODIFIED:new use case UC5

--- src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
@@ -4,33 +4,43 @@ public class EmployeeWageCalculator {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+	
 	public static void main(String args[]) {
 	
 	System.out.println("*-------------Welcome to Employee Wage Computation Program-------------*");
 	
 	
 	//variables
-	int empHours=0;
-	int empWage=0;
+	int empHours=0,empWage=0,totalEmpWage=0;
 	
 	//Computations
-	int empCheck=(int)Math.floor(Math.random()*10)%3;
-	
-	switch(empCheck) {
-	
-	case IS_FULL_TIME:
-		empHours=8;
-		break;
-	case IS_PART_TIME:
-		empHours=4;
-		break;
-	default:
-		empHours=0;
-	
+	for(int i=0;i<NUM_OF_WORKING_DAYS;i++) {
+		
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		
+		switch(empCheck) {
+		
+			case IS_FULL_TIME:
+				empHours=8;
+				break;
+			case IS_PART_TIME:
+				empHours=4;
+				break;
+			default:
+				empHours=0;
+		
+			
+		}
+		empWage=empHours*EMP_RATE_PER_HOUR;
+		System.out.println("Employee wage:"+empWage);
+		totalEmpWage+=empWage;
+		
 	}
 	
-	empWage=empHours*EMP_RATE_PER_HOUR;
-	System.out.println("Employee wage:"+empWage);
+	
+	
+	System.out.println("Total Employee wage:"+totalEmpWage);
 	
 	}
 	
