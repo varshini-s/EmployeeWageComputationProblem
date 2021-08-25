b96ba22e516e7e56d82ebbc9dcc0e1c1d2aadbec
varshini <dreamvarshini@gmail.com>
2021-08-25 16:46:49 +0530

    REFACTORED:code refactored with class methods

--- src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
+++ src/com/bridgelabz/employeewageproblem/EmployeeWageCalculator.java
@@ -1,6 +1,10 @@
 package com.bridgelabz.employeewageproblem;
 
 public class EmployeeWageCalculator {
+	
+	int empHours,totalEmpWage,totalEmpHours,totalWorkDays;
+
+	
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
@@ -8,43 +12,71 @@ public class EmployeeWageCalculator {
 	public static final int MAX_WORK_HOURS=100;
 
 	
-	public static void main(String args[]) {
+	public EmployeeWageCalculator() 
 	
-	System.out.println("*-------------Welcome to Employee Wage Computation Program-------------*");
-	
-	
-	//variables
-	int empHours=0,totalEmpWage=0,totalEmpHours=0,totalWorkDays=0;
-	
-	//Computations
-	while(totalEmpHours<=MAX_WORK_HOURS && totalWorkDays<NUM_OF_WORKING_DAYS){
+	{
+		empHours=0;
+		totalEmpWage=0;
+		totalEmpHours=0;
+		totalWorkDays=0;
 		
-		totalWorkDays++;
 		
+	}
+	
+	public static void checkEmployeeType(EmployeeWageCalculator Employee)
+	{
 		int empCheck=(int)Math.floor(Math.random()*10)%3;
 		
 		switch(empCheck) {
 		
 			case IS_FULL_TIME:
-				empHours=8;
+				Employee.empHours=8;
 				break;
 			case IS_PART_TIME:
-				empHours=4;
+				Employee.empHours=4;
 				break;
 			default:
-				empHours=0;
+				Employee.empHours=0;
 		
 			
 		}
-		totalEmpHours+=empHours;
-		System.out.println("Day#:"+totalWorkDays+" Emp hours:"+empHours);
-		
 		
 	}
 	
+	public static void calculateEachDayWage(EmployeeWageCalculator Employee)
+	{
+		while(Employee.totalEmpHours<=MAX_WORK_HOURS && Employee.totalWorkDays<NUM_OF_WORKING_DAYS)
+		{
+			
+			Employee.totalWorkDays++;
+			checkEmployeeType(Employee);
+			
+			Employee.totalEmpHours+=Employee.empHours;
+			System.out.println("Day#:"+Employee.totalWorkDays+" Emp hours:"+Employee.empHours);
+			
+			
+		}
+	}
 	
-	 totalEmpWage=totalEmpHours*EMP_RATE_PER_HOUR;
-	System.out.println("Total Employee wage:"+totalEmpWage);
+	public static void calculateEachMonthWage(EmployeeWageCalculator Employee)
+	{
+		Employee.totalEmpWage=Employee.totalEmpHours*EMP_RATE_PER_HOUR;
+		
+	}
+	
+	public static void main(String args[]) 
+	{
+	
+	System.out.println("*-------------Welcome to Employee Wage Computation Program-------------*");
+	
+	EmployeeWageCalculator Employee1 = new EmployeeWageCalculator();
+	
+	//variables
+	calculateEachDayWage(Employee1);
+	calculateEachMonthWage(Employee1);
+	
+	System.out.println("Total Employee wage:"+Employee1.totalEmpWage);
+	
 	
 	}
 	
